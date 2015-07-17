f8c9cb6335bbb33c973ff2d827585e29b4112eb7
sippo131 <sippo_131@yahoo.co.jp>
2015-07-17 10:40:18 +0900

    succeded in db:migrate in story

--- db/migrate/20150717011150_create_stories.rb
+++ db/migrate/20150717011150_create_stories.rb
@@ -1,9 +1,10 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-      t.column :title, :text
-      t.column :author, :text
+      t.column :title, :string
+      t.column :author, :string
       t.column :summary, :text
+      t.column :url, :string
 
       t.timestamps null: false
     end
