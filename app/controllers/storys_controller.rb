bf3a208afd6c9ddad4e3593af17a8b5751589391
sippo131 <sippo_131@yahoo.co.jp>
2015-07-17 14:12:56 +0900

    added the editpage and new page

--- app/controllers/storys_controller.rb
+++ app/controllers/storys_controller.rb
@@ -8,4 +8,12 @@ class StorysController < ApplicationController
 		@persons = @story.person.all
 	end
 
+	def edit
+		@story = Story.find(params[:id])
+	end
+
+	def new
+		@story = Story.new
+	end
+
 end
